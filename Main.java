2ece109de482bc840c487d436653471f73018433
Pako-12317 <patrykm10@nycstudents.net>
2024-11-04 14:31:27 +0000

    commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("What number do you want to factorial? ");
+    System.out.print("Enter a number between 0 and 170: ");
+    int userNumber = scanner.nextInt();
+    
+    
+  }
+}
