cd473ff5d6afd6d781f20e0dcdc9b9ac4fc91810
Pako-12317 <patrykm10@nycstudents.net>
2024-11-07 13:52:54 +0000

    ,commit

--- U4T2/StringLoops.java
+++ U4T2/StringLoops.java
@@ -35,5 +35,38 @@ public class StringLoops {
       StringBuilder str = new StringBuilder(origString);
       return str.reverse().toString();
     }
+
+        /* Returns a String with all instances of "searchChar" in "origString" replaced with
+     "replaceChar"; matches should be case sensitive (i.e. no need to worry about
+      lowercase vs. uppercase)
+     
+     - if searchChar = "a", origString = "canned beans and bananas", replaceChar = "*"
+       this method returns "c*nned be*ns *nd b*n*n*s" (A not a match because of case)
+     - if searchChar = "t", origString = "Ratatattat", replaceChar = "p"
+       this method returns "Rapapappap"
+     - if searchChar = "!", origString = "Nice! Day today!", replaceChar = "?"
+       this method returns "Nice? Day today?"
+     - if searchChar = "i", origString = "it’s the brain drain pain train",
+       replaceChar = "I", this method returns "It’s the braIn draIn paIn traIn"
+     - if searchChar = "i", origString = "it’s the brain drain pain train",
+       replaceChar = " ", this method returns " t’s the bra n dra n pa n tra n"
+     - if searchChar = "A", origString = "ABABABABABC", replaceChar = "D"
+       this method returns "DBDBDBDBDBC"
+     - if searchChar = "a", origString = "ABABABABABC", replaceChar = "D"
+       this method returns "ABABABABABC" (no matches because of case)
+
+    PRECONDITION: searchChar and replaceChar are single characters and are not equal
+    */  
+  public String replaceCharacterV1(String searchChar, String origStr, String replaceChar){
+    if(countCharacters(searchChar, origStr)>0){
+      for(int i=0;i<origStr.length();i++){
+        
+      }
+    }
+    
+    return "";
+   }
+ 
+ 
   }
   
